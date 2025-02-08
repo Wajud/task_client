e98c04b1027412404db5babceedcddf570a9246b
Wajud <wajudkareem757@gmail.com>
2025-02-09 00:11:29 +0100

    added create todo page

---
+++ src/app/edit/[id]/page.jsx
@@ -0,0 +1,140 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
+
+const page = () => {
+  const [todo, setTodo] = useState(null);
+  const [title, setTitle] = useState("");
+  const [isCompleted, setIsCompleted] = useState(false);
+  const [titleError, setTitleError] = useState(false);
+  const [sentStatus, setSentStatus] = useState(false);
+
+  console.log(isCompleted);
+
+  const pathName = usePathname();
+  const id = pathName.split("/")[2];
+  console.log(id);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/api/todos/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setTodo(data.data);
+      })
+      .catch((err) => console.log("Something went wrong: ", err));
+  }, []);
+
+  const sendTodo = (e) => {
+    e.preventDefault();
+    if (!title) {
+      setTitleError(true);
+      setTimeout(() => setTitleError(false), 2000);
+      return;
+    }
+    const todo = { title, isCompleted };
+    fetch("http://localhost:8080/api/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data), setSentStatus(true);
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className="flex justify-center  pt-12">
+      {todo && (
+        <form
+          className="w-[80%] max-w-[40rem] border border-gray-200 rounded-md shadow-md py-12 flex flex-col items-center"
+          onSubmit={sendTodo}
+        >
+          <input
+            type="text"
+            placeholder={todo.title}
+            className="px-2 py-1 block w-96 border border-gray-300 focus:outline rounded-sm mb-2"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          {/* {titleError && (
+      <p className="text-center text-red-500 -mt-2 mb-2">
+      Todo title must be set
+      </p>
+   )} */}
+          <div>
+            <input
+              type="checkbox"
+              id="isCompleted"
+              value={todo.isCompleted}
+              onChange={() => {
+                setIsCompleted(!todo.isCompleted);
+                console.log("isCompleted: ", todo.isCompleted);
+              }}
+              className="mb-2 hidden"
+            />
+            <label htmlFor="isCompleted" className="flex gap-1 text-blue-500">
+              {" "}
+              {todo.isCompleted ? (
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    x="2"
+                    y="2"
+                    width="20"
+                    height="20"
+                    rx="4"
+                    stroke="#4561ed"
+                    strokeWidth="2"
+                    fill="none"
+                  />
+                  <path
+                    d="M6 12L10 16L18 8"
+                    stroke="#4561ed"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    x="2"
+                    y="2"
+                    width="20"
+                    height="20"
+                    rx="4"
+                    stroke="#4561ed"
+                    strokeWidth="2"
+                  />
+                </svg>
+              )}
+              completed
+            </label>
+          </div>
+          <button className="block w-fit mx-auto mt-10 px-4 py-1 bg-green-500 rounded-sm text-white text-lg hover:shadow-md">
+            Save
+          </button>
+          {sentStatus && (
+            <p className="text-center text-green-500 mb-2">Todo updated.</p>
+          )}
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default page;
--- src/app/page.js
+++ src/app/page.js
@@ -4,26 +4,6 @@ import Link from "next/link";
 
 export default function Home() {
   const [todos, setTodos] = useState(null);
-  // const [isTodosReady]
-
-  // const dummyTodos = [
-  //   {
-  //     title: "Wash clothes",
-  //     isCompleted: true,
-  //   },
-  //   {
-  //     title: "Cook food",
-  //     isCompleted: false,
-  //   },
-  //   {
-  //     title: "Play games",
-  //     isCompleted: false,
-  //   },
-  //   {
-  //     title: "Do NYSC",
-  //     isCompleted: true,
-  //   },
-  // ];
 
   useEffect(() => {
     fetch("http://localhost:8080/api/todos")
@@ -96,20 +76,23 @@ export default function Home() {
                 </div>
                 <div className="flex gap-1">
                   {/* Edit icon */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="size-5 cursor-pointer"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                    />
-                  </svg>
+
+                  <Link href={`/edit/${todo._id}`}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-5 cursor-pointer"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                      />
+                    </svg>
+                  </Link>
 
                   {/* Delete Icon */}
                   <svg
